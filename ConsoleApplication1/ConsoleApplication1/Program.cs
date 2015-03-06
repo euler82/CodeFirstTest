650d04147c8647c8d42b88f7867683e5bbef6556
euler91 <euler91@sohu.com>
2015-03-06 13:35:30 +0800

    Create

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -4,12 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity.Infrastructure; 
 namespace ConsoleApplication1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            using (var db = new BlogContext())
+            {
+                db.Blogs.Add(new Blog { Name = "Another Blog " });
+                db.SaveChanges();
+
+                foreach (var blog in db.Blogs)
+                {
+                    Console.WriteLine(blog.Name);
+                }
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
         }
+    }
+
+    public class BlogContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+    }
+
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Name { get; set; }
     }
 }
